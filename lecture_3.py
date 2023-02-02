c34fdd346f56d51190f1786804ea3d8ff603d739
roangira99 <angirarobert7@gmail.com>
2023-02-02 10:27:47 +0300

    lecture_3.py

---
+++ lecture_3.py
@@ -0,0 +1,24 @@
+# Text, numbers and print formatting
+# print(type("90"))
+# result = "90" + '90'
+# print(result)
+
+# Type casting
+# result = int("90") + int('90')
+# print(result)
+# print(type(result))
+
+# num_1 = 30
+# num_2 = 40
+# result = num_1 + num_2
+# result = str(result)
+# print(result)
+# print(type(result))
+
+# Getting input from user
+print("Welcome to the multiplication program")
+print("-"*30)
+num_1 = int(input("Enter a number to multiply-> "))
+num_2 = int(input("Enter a second number to multiply-> "))
+result = num_1 * num_2
+print(result)
