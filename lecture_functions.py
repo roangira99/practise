2bb0372996acb8f039d8c0125944a5b63e8670f4
roangira99 <angirarobert7@gmail.com>
2023-02-13 08:31:40 +0300

    Functions

---
+++ lecture_functions.py
@@ -0,0 +1,56 @@
+# def get_input_from_user():
+#     name_result = input("Enter your response here -> ")
+#     print(f"Your response was {name_result}")
+#
+# print("Welcome to the program, what is your name?")
+# get_input_from_user()
+#
+# print("What did you think of the food you ate today?")
+# get_input_from_user()
+#
+# returning a value
+# def get_input_from_user():
+#     result = input("Enter your response here -> ")
+#     return f"Your response was {result}"
+#
+# print("Welcome to the program, what is your name?")
+# print(get_input_from_user())
+#
+# print("What did you think of the food you ate today?")
+# print(get_input_from_user())
+
+# returning different values from a user
+# def get_input_from_user():
+#     result = input("Enter your response here -> ")
+#     return result
+#
+# print("Welcome to the program, what is your name?")
+# name_result = get_input_from_user()
+#
+# print("What did you think of the food you ate today?")
+# food_result = get_input_from_user()
+#
+# print("What tv show ending did you dislike the most?")
+# show_result = get_input_from_user()
+#
+# print(f"To summarize: Your name is {name_result}, \
+# you ate {food_result} food today and hated the ending\
+#  of {show_result}")
+
+# # We can directly return the string that the user inputs
+# def get_input_from_user():
+#     return input("Enter your response here -> ")
+#
+# print("Welcome to the program, what is your name?")
+# name_result = get_input_from_user()
+#
+# print("What did you think of the food you ate today?")
+# food_result = get_input_from_user()
+#
+# print("What tv show ending did you dislike the most?")
+# show_result = get_input_from_user()
+#
+# # The benefit of doing this is that you can run other functions on your objects as shown below
+# print(f"To summarize: Your name is {name_result.capitalize()}, \
+# you ate {food_result} food today and hated the ending\
+#  of {show_result.upper()}")
