909ea380f5d28b0ac824f91037115fa8a01af92b
roangira99 <angirarobert7@gmail.com>
2023-03-06 09:02:01 +0300

    Fibonacci sequence with recurssion

---
+++ fibonacci_recur.py
@@ -0,0 +1,18 @@
+def fib_recur(n):
+    # building the base cases i.e. 0 and 1
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib_recur(n-1) + fib_recur(n-2)
+
+def fib_runner(z):
+    print(f"The {z}th number in the fibonacci sequence is {fib_recur(z)}")
+
+z = 0
+fib_runner(z)
+z = 1
+fib_runner(z)
+z = 10
+fib_runner(z)
